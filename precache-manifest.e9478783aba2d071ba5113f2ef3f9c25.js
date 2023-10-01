cef24bd9b929a3e128f7a182dfcd02ccb397e2ac
feiaunicamp <inscricoes.feia@gmail.com>
2023-09-30 23:04:49 -0300

    Updates

---
+++ precache-manifest.e9478783aba2d071ba5113f2ef3f9c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e7c46e7777c2d1355f31",
+    "url": "/static/css/main.e8d27ec1.chunk.css"
+  },
+  {
+    "revision": "e7c46e7777c2d1355f31",
+    "url": "/static/js/main.33ac2b40.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c74e716e9a60e5dd027b",
+    "url": "/static/js/2.80949cdd.chunk.js"
+  },
+  {
+    "revision": "1b5bdde731ad9ffbfa96b51153a7dee8",
+    "url": "/static/media/bg_feia.1b5bdde7.png"
+  },
+  {
+    "revision": "f259b875a35399b46e6627c612166476",
+    "url": "/index.html"
+  }
+];
